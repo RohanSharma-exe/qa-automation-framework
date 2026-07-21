d60797232f9a83691079d530ea24d629079fa83b
ROHAN <rohan.sharma1234987650@gmail.com>
2026-07-21 20:50:01 +0530

    Resolove pipeline error

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,11 @@
 from pathlib import Path
+import os
 
 BASE_URL = "https://www.saucedemo.com/"
 
 TIMEOUT = 10
 
-HEADLESS = False
+HEADLESS = os.getenv("HEADLESS", "True").lower() == "true"
 
 ROOT = Path(__file__).resolve().parent.parent
 SCREENSHOT_DIR = ROOT / "screenshots"
--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 from config.settings import HEADLESS
 
@@ -10,12 +8,14 @@ def get_driver():
 
     if HEADLESS:
         options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--window-size=1920,1080")
 
-    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=options)
 
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options,
-    )
+    if not HEADLESS:
+        driver.maximize_window()
 
     return driver
